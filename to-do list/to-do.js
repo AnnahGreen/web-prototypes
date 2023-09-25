0ad48ee5404030fd6c6d89a94d70f8687c260b9a
Annah Green <ajgreen@my.loyno.edu>
2023-09-25 00:17:39 -0400

    P element as list content.
    In case I want to come back to this set up.

--- to-do list/to-do.js
+++ to-do list/to-do.js
@@ -6,37 +6,43 @@ console.log(addBtn);
 
 addBtn.addEventListener('click', function(){
     // Initialize item on to do list from user input inside text box
-    //console.log("Button clicked.");
+
     // Creating container to hold new list element.
     let listItem = document.createElement('li');
-    // works: console.log("listItem:", listItem);
     listItem.classList.add('list-item');
-    listItem.innerHTML = inputBar.value;
 
-    // 'Done" and 'Remove' buttons to the right.
+    // Animation to reveal to-do on list once user has clicked the 'add' button.
+    let reveal_todo = document.createElement('span');
+    reveal_todo.classList.add('reveal-panel');
+    reveal_todo.innerHTML = "a";  /* "a" used here as filler text so that the bar wil show */
+    listItem.appendChild(reveal_todo);
+    
+    // Append p element with user input as value.
+    let listContent = document.createElement('p');
+    listContent.classList.add('list-item-text');
+    listContent.innerHTML = inputBar.value;
+    listItem.appendChild(listContent);
+
+    // 'Done" and 'Remove' buttons to the right. 
   
     /* 'Remove' button. */ let deleteBtn = document.createElement('button');
     //console.log("check: ", deleteBtn);
     deleteBtn.classList.add('material-symbols-outlined');
     deleteBtn.classList.add('delete');
     deleteBtn.innerHTML = "delete";
-    listItem.appendChild(deleteBtn); 
+    listContent.appendChild(deleteBtn); 
     /* 'Done' button. */ let checkBtn = document.createElement('button');
     //console.log("check: ", checkBtn);
     checkBtn.classList.add('material-symbols-outlined')
     checkBtn.classList.add('complete');
     checkBtn.innerHTML = "done";
-    listItem.appendChild(checkBtn);   
+    listContent.appendChild(checkBtn);   
 
     // Add new to-do to list and clear user input from bar.
     toDoList.appendChild(listItem);
     inputBar.value = "";
 
-    // Animation to reveal to-do on list once user has clicked the 'add' button.
-    let reveal_todo = document.createElement('span');
-    reveal_todo.classList.add('reveal-panel');
-    reveal_todo.innerHTML = "Filler text. Won't display on box. Necessary to initialize.";
-    listItem.appendChild(reveal_todo);
+    
 
     // Add listeners to 'Done' and 'Remove' buttons.
     checkBtn.addEventListener('click', function() {
