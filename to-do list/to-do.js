e14adc0ea9d60612ab2243c7f8271551a25972f5
Annah Green <ajgreen@my.loyno.edu>
2023-10-03 03:15:45 -0400

    Probably close to last update.
    Session today: probably 5 hours trying to get a feature to work and then giving up and adding 5 other features in a fraction of that time. Updates inlclude: strike through when check is pressed, dif. color/rounded reveal bar, "Enter" key functionality,  error for attempting to add blank to-do.

--- to-do list/to-do.js
+++ to-do list/to-do.js
@@ -3,66 +3,77 @@ let inputBar = document.getElementById("input-ToDo");
 let addBtn = document.getElementById("add-button");
 
 
+function validateInput() {
+    if (inputBar.value === "") {
+        alert("Input field cannot be empty.");
+        return false;
+    }
+}
+
 addBtn.addEventListener('click', function(){
     // Initialize item on to do list from user input inside text box
+    let validatedInput = validateInput();
+    // Only add item to list if input bar has a value. Else display an alert() notification.
+    if (validatedInput !== false) {  /* Add input to list */
+        // Creating container to hold new list element.
+        let listItem = document.createElement('li');
+        listItem.classList.add('list-item');
+        //let listItemHeight = listItem.offsetHeight;
+
+        /* where span was previously */
+        
+        //console.log("parent is: ", reveal_todo.parentElement);
+        
+        // Append p element with user input as value.
+        let listContent = document.createElement('p');
+        listContent.classList.add('list-item-text');
+        listContent.innerHTML = inputBar.value;
+        listItem.appendChild(listContent);
+
+        // console.log("Height: ", listItem.offsetHeight)
+        // 'Done" and 'Remove' buttons to the right. 
     
+        /* 'Remove' button. */ let deleteBtn = document.createElement('button');
+        //console.log("check: ", deleteBtn);
+        deleteBtn.classList.add('material-symbols-outlined');
+        deleteBtn.classList.add('delete');
+        deleteBtn.innerHTML = "delete";
+        listItem.appendChild(deleteBtn); 
+        /* 'Done' button. */ let checkBtn = document.createElement('button');
+        //console.log("check: ", checkBtn);
+        checkBtn.classList.add('material-symbols-outlined')
+        checkBtn.classList.add('complete');
+        checkBtn.innerHTML = "done";
+        listItem.appendChild(checkBtn);   
 
-    // Creating container to hold new list element.
-    let listItem = document.createElement('li');
-    listItem.classList.add('list-item');
-    //let listItemHeight = listItem.offsetHeight;
+        // Add new to-do to list and clear user input from bar.
+        toDoList.appendChild(listItem);
+        inputBar.value = "";
 
-    /* where span was previously */
-    
-    //console.log("parent is: ", reveal_todo.parentElement);
-    
-    // Append p element with user input as value.
-    let listContent = document.createElement('p');
-    listContent.classList.add('list-item-text');
-    listContent.innerHTML = inputBar.value;
-    listItem.appendChild(listContent);
+        
+        // Animation to reveal to-do on list once user has clicked the 'add' button.
+        let reveal_todo = document.createElement('span');
+        reveal_todo.classList.add('reveal-panel');
+        reveal_todo.innerHTML = "a";  /* "a" used here as filler text so that the bar wil show */
+        let offsetHeight = listItem.offsetHeight;
+        let heightValue = offsetHeight.toString() + "px";
+        reveal_todo.style.height = heightValue;
+        listItem.appendChild(reveal_todo);
+        
 
-    // console.log("Height: ", listItem.offsetHeight)
-    // 'Done" and 'Remove' buttons to the right. 
-  
-    /* 'Remove' button. */ let deleteBtn = document.createElement('button');
-    //console.log("check: ", deleteBtn);
-    deleteBtn.classList.add('material-symbols-outlined');
-    deleteBtn.classList.add('delete');
-    deleteBtn.innerHTML = "delete";
-    listItem.appendChild(deleteBtn); 
-    /* 'Done' button. */ let checkBtn = document.createElement('button');
-    //console.log("check: ", checkBtn);
-    checkBtn.classList.add('material-symbols-outlined')
-    checkBtn.classList.add('complete');
-    checkBtn.innerHTML = "done";
-    listItem.appendChild(checkBtn);   
-
-    // Add new to-do to list and clear user input from bar.
-    toDoList.appendChild(listItem);
-    inputBar.value = "";
-
-    
-    // Animation to reveal to-do on list once user has clicked the 'add' button.
-    let reveal_todo = document.createElement('span');
-    reveal_todo.classList.add('reveal-panel');
-    reveal_todo.innerHTML = "a";  /* "a" used here as filler text so that the bar wil show */
-    let offsetHeight = listItem.offsetHeight;
-    let heightValue = offsetHeight.toString() + "px";
-    reveal_todo.style.height = heightValue;
-    listItem.appendChild(reveal_todo);
-
-    
-    
-
-    // Add listeners to 'Done' and 'Remove' buttons.
-    checkBtn.addEventListener('click', function() {
-        toDoList.removeChild(listItem);
-    });
-    deleteBtn.addEventListener('click', function() {
-        toDoList.removeChild(listItem);
-    });
-
+        // Add listeners to 'Done' and 'Remove' buttons.
+        checkBtn.addEventListener('click', function() {
+            listContent.style.textDecorationLine = "line-through";
+        });
+        deleteBtn.addEventListener('click', function() {
+            toDoList.removeChild(listItem);
+        });
+    }
     
 });
 
+inputBar.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addBtn.click();
+    }
+});
